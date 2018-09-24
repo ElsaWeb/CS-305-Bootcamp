0b33efa270af4eac0e2469f5a679305b41039515
Elsa Weber <elsa.weber@epfl.ch>
2018-09-24 17:33:46 +0200

    Implement a greeting feature

--- app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -1,13 +1,27 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "ch.epfl.sweng.bootcamp.MESSAGE"; //MESSAGE ??!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, GreetingActivity.class);
+        EditText editText = (EditText) findViewById(R.id.mainName);
+        String name = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, name);
+        startActivity(intent);
+
+    }
+
 }
